5813b5d791b1c6a315f58a80d7d214c5252e8883
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-02-21 00:28:40 +0300

    setup_mysql_dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,7 +7,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Creating the User
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Granting the privilages
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
