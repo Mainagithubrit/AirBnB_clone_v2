0d625fa1417cf15a4dce936867f14b1ad9c511c1
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-02-21 00:39:35 +0300

    setup_mysql_dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,14 +3,7 @@
 -- User: hbnb_dev.
 -- Password: hbnb_dev_pwd.
 
--- Creating the Database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Creating the User
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Granting the privilages
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-
--- Granting select privilages
-GRANT SELECT ON performance_schema . * 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
