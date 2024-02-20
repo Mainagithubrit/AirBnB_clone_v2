dace90612ec11d30b70777ff549dd074c3042714
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-02-20 22:22:14 +0300

    setup_mysql_dev

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,19 @@
+-- a script that prepares a MySQL server
+-- Database: hbnb_dev_db
+-- User: hbnb_dev
+-- Password: hbnb_dev_pwd
+
+-- Creating the Database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Creating the User
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Granting the privilages
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+
+-- Granting select privilages
+GRANT SELECT ON performance_schema.* 'hbnb_dev'@'localhost';
+
+-- Applying changes
+FLUSH PRIVILEGES;
