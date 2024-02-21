cbd6385fa78018ee861d706d3c9056a61a28c183
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-02-21 23:13:31 +0300

    Updated

--- models/state.py
+++ models/state.py
@@ -10,7 +10,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
