a147f9c75219310430124d045a339c4e48f058c3
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-02-22 12:47:27 +0300

    Updated

--- models/state.py
+++ models/state.py
@@ -7,14 +7,20 @@ from sqlalchemy import Column, Integer, String
 import models
 from models.city import City
 import shlex
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete, delete-orphan',
-                          backref="state")
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete, delete-orphan',
+                              backref="state")
+    else:
+        name = ""
 
     @property
     def cities(self):
