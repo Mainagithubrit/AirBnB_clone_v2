dd2770000cc9444113ab6fe8908ea12108995a7d
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-03-07 22:19:12 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ using the function do_deploy"""
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = [ '35.153.66.11', '100.26.154.224' ]
+env.hosts = ['35.153.66.11', '100.26.154.224']
 
 
 def do_deploy(archive_path):
@@ -17,13 +17,13 @@ def do_deploy(archive_path):
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}'.format(path, no_ext))
+        run('mkdir -p {}{}/'.format(path, no_ext))
         run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
         run('rm /tmp/{}'.format(file_n))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return Truue
+        return True
     except:
         return False
