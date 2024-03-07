ea80bcbb8b9388f1e73b8ec6febcb8da735550f9
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-03-07 23:14:27 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,5 +26,4 @@ def do_deploy(archive_path):
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except Exception as e:
-        print(f"An error occurred: {e}")
         return False
